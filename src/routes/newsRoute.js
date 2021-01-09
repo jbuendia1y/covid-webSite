36648f28a9b83f3d01a6608ef40e0e79d94cb596
jbuendia1y <jgamer669@gmail.com>
2021-01-09 18:02:38 -0500

    news

--- src/routes/newsRoute.js
+++ src/routes/newsRoute.js
@@ -6,14 +6,14 @@ const fetch = require('node-fetch')
 const {API_NEWS_KEY} = process.env
 
 router.get('/news',async (req,res)=>{
-    const data = await fetch('https://covid-19-news.p.rapidapi.com/v1/covid?q=covid&lang=es&media=True',{
+    const data = await fetch('https://covid-19-news.p.rapidapi.com/v1/covid?q=covid&lang=es&from=2020-05-01&topic=world&media=True',{
         method : 'GET',
         headers : {
             "x-rapidapi-key": API_NEWS_KEY,
             "x-rapidapi-host": "covid-19-news.p.rapidapi.com",
             "useQueryString": true
         }
-    }) .then(res => res.json())
+    })  .then(res => res.json())
         .then(data => data.articles)
 
     res.render('news.ejs',{name : 'NEWS | Covid-19 WebSite',data})
